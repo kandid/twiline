4e19fb9d11ccaadf6f1d63189f57da855bff569f
dominik <dominik@localhost>
2014-04-15 20:35:44 +0000

    *fixed: add missing SpringUtiliities method
    svn path=/trunk/Twiline/; revision=1650

--- src/de/kandid/apps/twiline/OptionDialog.java
+++ src/de/kandid/apps/twiline/OptionDialog.java
@@ -19,6 +19,8 @@
 package de.kandid.apps.twiline;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
@@ -29,11 +31,11 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.Spring;
+import javax.swing.SpringLayout;
 
 import de.kandid.ui.Action;
 import de.kandid.ui.Keys;
-import de.kandid.ui.swing.SpringLayout;
-import de.kandid.ui.swing.SpringUtilities;
 
 public class OptionDialog extends JDialog {
 
@@ -51,7 +53,7 @@ public class OptionDialog extends JDialog {
 			phrases.add(new de.kandid.model.types.Boolean.View(pm._bold));
 			phraseModels.add(pm);
 		}
-      SpringUtilities.makeCompactGrid(phrases, model._value._phrases.length + 1, 3, 0, 0, 5, 5);
+      makeCompactGrid(phrases, model._value._phrases.length + 1, 3, 0, 0, 5, 5);
       panel.add(phrases, BorderLayout.CENTER);
 
       JPanel buttons = new JPanel(new FlowLayout(FlowLayout.TRAILING));
@@ -76,5 +78,85 @@ public class OptionDialog extends JDialog {
       panel.add(buttons, BorderLayout.SOUTH);
 		add(panel);
       pack();
+	}
+
+	/**
+	 * Aligns the first <code>rows</code> * <code>cols</code>
+	 * components of <code>parent</code> in
+	 * a grid. Each component in a column is as wide as the maximum
+	 * preferred width of the components in that column;
+	 * height is similarly determined for each row.
+	 * The parent is made just big enough to fit them all.
+	 *
+	 * @param rows number of rows
+	 * @param cols number of columns
+	 * @param initialX x location to start the grid at
+	 * @param initialY y location to start the grid at
+	 * @param xPad x padding between cells
+	 * @param yPad y padding between cells
+	 */
+	public static void makeCompactGrid(Container parent,
+	                                   int rows, int cols,
+	                                   int initialX, int initialY,
+	                                   int xPad, int yPad) {
+		SpringLayout layout;
+		try {
+			layout = (SpringLayout)parent.getLayout();
+		} catch (ClassCastException exc) {
+			System.err.println("The first argument to makeCompactGrid must use SpringLayout.");
+			return;
+		}
+
+		//Align all cells in each column and make them the same width.
+		Spring x = Spring.constant(initialX);
+		for (int c = 0; c < cols; c++) {
+			Spring width = Spring.constant(0);
+			for (int r = 0; r < rows; r++) {
+				width = Spring.max(width,
+						getConstraintsForCell(r, c, parent, cols).
+								getWidth());
+			}
+			for (int r = 0; r < rows; r++) {
+				SpringLayout.Constraints constraints =
+						getConstraintsForCell(r, c, parent, cols);
+				constraints.setX(x);
+				constraints.setWidth(width);
+			}
+			x = Spring.sum(x, Spring.sum(width, Spring.constant(xPad)));
+		}
+
+		//Align all cells in each row and make them the same height.
+		Spring y = Spring.constant(initialY);
+
+		for (int r = 0; r < rows; r++) {
+			Spring height = Spring.constant(0);
+			for (int c = 0; c < cols; c++) {
+				height = Spring.max(height,
+						getConstraintsForCell(r, c, parent, cols).
+								getHeight());
+			}
+			for (int c = 0; c < cols; c++) {
+				SpringLayout.Constraints constraints =
+						getConstraintsForCell(r, c, parent, cols);
+				constraints.setY(y);
+				constraints.setHeight(height);
+			}
+			y = Spring.sum(y, Spring.sum(height, Spring.constant(yPad)));
+		}
+
+		//Set the parent's size.
+		SpringLayout.Constraints pCons = layout.getConstraints(parent);
+		pCons.setConstraint(SpringLayout.SOUTH, y);
+		pCons.setConstraint(SpringLayout.EAST, x);
+	}
+
+	/* Used by makeCompactGrid. */
+	private static SpringLayout.Constraints getConstraintsForCell(
+			int row, int col,
+			Container parent,
+			int cols) {
+		SpringLayout layout = (SpringLayout) parent.getLayout();
+		Component c = parent.getComponent(row * cols + col);
+		return layout.getConstraints(c);
 	}
 }
